395a359d48399ba8d4e1ca86d25aacf456fb65e5
Nayeli Arredondo <nayeli.arredondohuerta@pcc.edu>
2023-11-04 10:14:02 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by Nayeli.Arredondo on 11/04/23
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
