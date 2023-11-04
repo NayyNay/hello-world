3ca03899486fa2b944162e14132edb667190dac3
Nayeli Arredondo <nayeli.arredondohuerta@pcc.edu>
2023-11-04 10:16:53 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Nayeli.Arredondo on 11/04/23
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
